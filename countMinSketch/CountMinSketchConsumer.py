85ec12127f48ab69cc27e154a54a039c515f4b22
hvenugopalan <hkrishnan1995@gmail.com>
2019-12-03 20:58:58 -0600

    Changes to count min sketch consumer

--- countMinSketch/CountMinSketchConsumer.py
+++ countMinSketch/CountMinSketchConsumer.py
@@ -5,21 +5,23 @@ import datetime
 from kafka.structs import TopicPartition, OffsetAndTimestamp
 import sys
 from CountMinSketch import HeavyHitters
-
+import operator
+from memory_profiler import profile
 
 class CountMinSketchConsumer:
 
+    @profile
     def consumerMessage(self, use_case):
         print("inside consumer")
         self.consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'])
         if use_case == "HashTags":
             # get current offset and store it and process unitl offset reaches this value
-            self.tp = TopicPartition("test", 0)
+            self.tp = TopicPartition("test2", 0)
             self.cur_offset = self.consumer.end_offsets([self.tp])
 
             # timestamp corresponding to current time - 24 hours
             current_time = datetime.datetime.now()
-            old_time = current_time - datetime.timedelta(hours=48)
+            old_time = current_time - datetime.timedelta(minutes=20)
             old_epoch_ts = int(old_time.timestamp() * 1000)  # in miliseconds
 
             # get the offset corresponding to old timestamp
@@ -35,19 +37,19 @@ class CountMinSketchConsumer:
 
             # create instance of misra-gries algo
             self.count_min_sketch = HeavyHitters(
-                width=100, depth=100, num_hitters=1)
+                width=100, depth=100, num_hitters=10)
 
         elif use_case == "UserMention":
 
             # get current offset
-            self.tp = TopicPartition("test", 0)
+            self.tp = TopicPartition("test3", 0)
             self.cur_offset = self.consumer.end_offsets([self.tp])
             print("cur offset", self.cur_offset[self.tp])
 
             # timestamp corresponds to cur time - 24 hours
             cur_time = int(round(time.time() * 1000))
             current_time = datetime.datetime.now()
-            old_time = current_time - datetime.timedelta(minutes=15)
+            old_time = current_time - datetime.timedelta(minutes=20)
             old_epoch_ts = int(old_time.timestamp() * 1000)  # in miliseconds
 
             # get the offset corresponds to old timestamp
@@ -63,7 +65,7 @@ class CountMinSketchConsumer:
 
             # create instance of misra-gries algo
             self.count_min_sketch = HeavyHitters(
-                width=100, depth=100, num_hitters=1)
+                width=100, depth=100, num_hitters=10)
 
         for message in self.consumer:
             '''
@@ -74,23 +76,23 @@ class CountMinSketchConsumer:
 
             r_msg = str(message.value.decode("utf-8"))
             tweet_text = json.loads(r_msg)
-
-            if tweet_text['tweet_data'] is not None and 'entities' in tweet_text['tweet_data'] and 'hashtags' in tweet_text['tweet_data']['entities']:
-                if use_case == "HashTags":
-                    eois = tweet_text['tweet_data']['entities']['hashtags']
-                else:
-                    eois = tweet_text['tweet_data']['entities']['user_mentions']
-                if len(eois) > 0:
-                    for eoi in eois:
-                        if use_case == "HashTags":
-                            eoi = eoi['text']
-                        else:
-                            eoi = eoi['name']
-                        print(eoi, "\n")
-                        self.count_min_sketch.add_alt(
-                            eoi, self.count_min_sketch.hashes(eoi, 100), 1)
+            print("Tweet text: ", tweet_text)
+            # if tweet_text['tweet_data'] is not None and 'entities' in tweet_text['tweet_data'] and 'hashtags' in tweet_text['tweet_data']['entities']:
+            #     if use_case == "HashTags":
+            #         eois = tweet_text['tweet_data']['entities']['hashtags']
+            #     else:
+            #         eois = tweet_text['tweet_data']['entities']['user_mentions']
+            #     if len(eois) > 0:
+            #         for eoi in eois:
+            #             if use_case == "HashTags":
+            #                 eoi = eoi['text']
+            #             else:
+            #                 eoi = eoi['name']
+            #             print(eoi, "\n")
+            self.count_min_sketch.add_alt(tweet_text, self.count_min_sketch.hashes(tweet_text, 100), 1)
 
         msg_frequent_item = self.count_min_sketch.heavyhitters
+        msg_frequent_item = sorted(msg_frequent_item.items(), key=operator.itemgetter(1), reverse=True)
         print("Most frequent item: ", msg_frequent_item)
       
 
